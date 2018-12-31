326e6a5ac8540d40941c9456a5ab07f00a68ed55
bluewind <zero18030@gmail.com>
2018-12-31 17:59:49 +0900

    名前変更 タブについて

--- app/src/main/java/momonyan/contactlensmanage/TabAdapter.kt
+++ app/src/main/java/momonyan/contactlensmanage/TabAdapter.kt
@@ -3,15 +3,15 @@ package momonyan.contactlensmanage
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import momonyan.contactlensmanage.Fragment.Tab1Fragment
-import momonyan.contactlensmanage.Fragment.Tab2Fragment
+import momonyan.contactlensmanage.fragment.TabMain
+import momonyan.contactlensmanage.fragment.TabSetting
 
 class TabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         val setFragment: Fragment
         when (position) {
-            0 -> setFragment = Tab1Fragment()
-            1 -> setFragment = Tab1Fragment()
+            0 -> setFragment = TabMain()
+            1 -> setFragment = TabSetting()
             else -> error("TabError")
         }
         return setFragment
--- app/src/main/java/momonyan/contactlensmanage/fragment/TabMain.kt
+++ app/src/main/java/momonyan/contactlensmanage/fragment/TabMain.kt
@@ -1,17 +1,15 @@
-package momonyan.contactlensmanage.Fragment
+package momonyan.contactlensmanage.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import momonyan.contactlensmanage.R
 
-class Tab2Fragment : Fragment() {
+class TabMain : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater.inflate(R.layout.tab1_layout, container, false)
+        var view = inflater.inflate(R.layout.tab_main, container, false)
         return view
     }
 
