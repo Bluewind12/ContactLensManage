0949c587e3186bcbd028f6f09d8687e99b413d28
Bluewind12 <zero18030@gmail.com>
2019-03-29 23:50:10 +0900

    リスト表示のちょっとした変更

--- app/src/main/java/momonyan/contactlensmanage/fragment/TabMore.kt
+++ app/src/main/java/momonyan/contactlensmanage/fragment/TabMore.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.tab_more.view.*
+import momonyan.contactlensmanage.ContactDataHolder
 import momonyan.contactlensmanage.R
 
 class TabMore : Fragment() {
@@ -19,16 +20,33 @@ class TabMore : Fragment() {
         val view = inflater.inflate(R.layout.tab_more, container, false)
 
         sharedPreferences = activity!!.getSharedPreferences("Data", Context.MODE_PRIVATE)
+        //Holder
+        val contactHolder = ContactDataHolder
+        contactHolder.maker = sharedPreferences.getString("maker", getString(R.string.not_setting))
+        contactHolder.lens = sharedPreferences.getString("lens", getString(R.string.not_setting))
+        contactHolder.type = sharedPreferences.getString("type", getString(R.string.not_setting))
+        contactHolder.otherMemos = sharedPreferences.getString("other", getString(R.string.not_setting))
 
-        view.makerText.text = sharedPreferences.getString("maker", getString(R.string.not_setting))
-        view.lensText.text = sharedPreferences.getString("lens", getString(R.string.not_setting))
-        view.typeText.text = sharedPreferences.getString("type", getString(R.string.not_setting))
-        view.otherText.text = sharedPreferences.getString("other", getString(R.string.not_setting))
+        //Holder
+        val contactHolder2 = ContactDataHolder
+        contactHolder2.maker = sharedPreferences.getString("maker2", getString(R.string.not_setting))
+        contactHolder2.lens = sharedPreferences.getString("lens2", getString(R.string.not_setting))
+        contactHolder2.type = sharedPreferences.getString("type2", getString(R.string.not_setting))
+        contactHolder2.otherMemos = sharedPreferences.getString("other2", getString(R.string.not_setting))
 
-        view.makerText_2.text = sharedPreferences.getString("maker2", getString(R.string.not_setting))
-        view.lensText_2.text = sharedPreferences.getString("lens2", getString(R.string.not_setting))
-        view.typeText_2.text = sharedPreferences.getString("type2", getString(R.string.not_setting))
-        view.otherText_2.text = sharedPreferences.getString("other2", getString(R.string.not_setting))
+        //空白を変更する
+        checkHolder(contactHolder)
+        checkHolder(contactHolder2)
+
+        //表示
+        view.makerText.text = contactHolder.maker
+        view.lensText.text = contactHolder.lens
+        view.typeText.text = contactHolder.type
+        view.otherText.text = contactHolder.otherMemos
+        view.makerText_2.text = contactHolder2.maker
+        view.lensText_2.text = contactHolder2.lens
+        view.typeText_2.text = contactHolder2.type
+        view.otherText_2.text = contactHolder2.otherMemos
 
         if (sharedPreferences.getBoolean("LRSetting", false)) {
             view.imageView3.setImageResource(R.drawable.l_icon)
@@ -41,4 +59,18 @@ class TabMore : Fragment() {
         return view
     }
 
+    private fun checkHolder(contactHolder: ContactDataHolder) {
+        if (contactHolder.maker == "") {
+            contactHolder.maker = getString(R.string.not_setting)
+        }
+        if (contactHolder.lens == "") {
+            contactHolder.lens = getString(R.string.not_setting)
+        }
+        if (contactHolder.type == "") {
+            contactHolder.type = getString(R.string.not_setting)
+        }
+        if (contactHolder.otherMemos == "") {
+            contactHolder.otherMemos = getString(R.string.not_setting)
+        }
+    }
 }
