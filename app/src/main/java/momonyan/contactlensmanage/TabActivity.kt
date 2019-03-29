da9af6ae7e195bf70c6b18b34b1f37705e4817a9
Bluewind12 <zero18030@gmail.com>
2019-03-29 23:58:29 +0900

    Nendのアップデート 5.0.3 → 5.1.0

--- app/src/main/java/momonyan/contactlensmanage/TabActivity.kt
+++ app/src/main/java/momonyan/contactlensmanage/TabActivity.kt
@@ -12,7 +12,6 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
