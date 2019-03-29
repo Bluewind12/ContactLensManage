1358aec47c33c68f3f864fd7d765c838de32ba97
Bluewind12 <zero18030@gmail.com>
2019-03-29 23:50:25 +0900

    コミット忘れ Holder

---
+++ app/src/main/java/momonyan/contactlensmanage/ContactDataHolder.kt
@@ -0,0 +1,8 @@
+package momonyan.contactlensmanage
+
+object ContactDataHolder {
+    var maker: String? = ""
+    var lens: String? = ""
+    var type: String? = ""
+    var otherMemos: String? = ""
+}
